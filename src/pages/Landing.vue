4184a8fd189d23ce804d6841916be409b985915a
Ernesto Garmendia <egarmendia@litebox.ai>
2019-06-29 13:56:08 -0300

    Initial commit

---
+++ src/pages/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .homepage-container {
+
+  }
+
+</style>
