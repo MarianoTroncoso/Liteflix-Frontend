6efce28064c90c368577f9b6b2bb914542043f19
Mariano <petotronco@gmail.com>
2021-05-28 16:16:37 -0300

    dropdown menu on hover icon user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,26 +14,7 @@
       <img src="../../static/plus.svg" alt="" />
       <span>Agregar película</span>
     </button>
-    <!-- <div class="action">
-      <div class="profile" @click="menuToggle">
-        <img src="../../static/user-01.svg" alt="" />
-      </div>
-      <div class="menu" ref="menu">
-        <ul>
-          <li>
-            <img src="../../static/fill-1.svg" alt="" /><a href="#"
-              >Ernesto G…</a
-            >
-          </li>
-          <li>
-            <img src="../../static/fill-1.svg" alt="" /><a href="#">User 03</a>
-          </li>
-          <li>
-            <img src="../../static/fill-1.svg" alt="" /><a href="#">User 04</a>
-          </li>
-        </ul>
-      </div>
-    </div> -->
+    <!-- RIGHT ITEMS -->
     <div class="right-items">
       <ul>
         <li><a href="#">Niños</a></li>
@@ -44,7 +25,52 @@
           ></a>
         </li>
         <li>
-          <a href="#"><img src="../../static/user-01.svg" alt="" /></a>
+          <div class="action">
+            <div class="profile" @mouseover="menuShow">
+              <img src="../../static/user-01.svg" alt="" />
+            </div>
+            <div class="menu" ref="menu">
+              <!-- perfiles -->
+              <ul class="ul-perfiles">
+                <li>
+                  <button class="selected-user-button">
+                    <div class="selected-user-image">
+                      <img src="../../static/fill-1.svg" alt="" />
+                    </div>
+                    <span>Ernesto G…</span>
+                  </button>
+                </li>
+                <li>
+                  <button class="not-selected-user-button">
+                    <div class="not-selected-user-image">
+                      <img src="../../static/fill-2.svg" alt="" />
+                    </div>
+                    <span>User 03</span>
+                  </button>
+                </li>
+                <li>
+                  <button class="not-selected-user-button">
+                    <div class="not-selected-user-image">
+                      <img src="../../static/fill-2.svg" alt="" />
+                    </div>
+                    <span>User 04</span>
+                  </button>
+                </li>
+              </ul>
+              <!-- settings -->
+              <ul class="ul-settings">
+                <li>
+                  <a href="#">Configuración</a>
+                </li>
+                <li>
+                  <a href="#">Ayuda</a>
+                </li>
+                <li>
+                  <a href="#"><b>Log out</b></a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
         <li>
           <a href="#"><img src="../../static/arrow.svg" alt="" /></a>
@@ -58,10 +84,14 @@
 export default {
   name: "Navbar",
   methods: {
-    menuToggle() {
-      const toggleMenu = this.$refs.menu;
-      toggleMenu.classList.toggle("active");
-    },
+    // menuShow() {
+    //   const toggleMenu = this.$refs.menu;
+    //   toggleMenu.classList.toggle("active");
+    // },
+    // menuHide() {
+    //   const toggleMenu = this.$refs.menu;
+    //   toggleMenu.classList.remove("active");
+    // },
   },
 };
 </script>
@@ -91,7 +121,6 @@ a {
   line-height: normal;
   letter-spacing: normal;
 }
-
 .add-movie-button span {
   max-width: 0;
   -webkit-transition: max-width 1s;
@@ -123,6 +152,12 @@ a {
 .right-items {
   margin-left: auto;
   display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  width: auto;
+}
+.right-items > ul {
+  display: inline-flex;
 }
 .right-items li {
   margin-left: 20px;
@@ -149,15 +184,23 @@ a {
   left: 8px;
 }
 // ---------------------------- TOGGLE MENU ----------------------------
-/*
+
 .action {
-  margin: 0px;
+  margin: 5px 5px 0 0;
   padding: 0;
-  position: fixed;
-  top: 20px;
-  right: 30px;
-  background-color: red;
+  // border: 2px solid red;
+  //position: absolute;
+  top: 0;
+  right: 0;
+  // background-color: red;
 }
+
+.action:hover .menu {
+  top: 50px;
+  visibility: visible;
+  opacity: 1;
+}
+
 .action .profile {
   position: relative;
   width: 25px;
@@ -176,22 +219,22 @@ a {
 }
 .action .menu {
   position: absolute;
-  top: 120px;
-  right: -10px;
-  padding: 5px 10px;
+  top: 20px;
+  right: -8px;
+  padding: 11px 9px 4px 10px;
   background-color: #fff;
-  width: 130px;
+  width: 100%;
   box-sizing: 0 5px 25px rgba(0, 0, 0, 0.1);
-  border-radius: 15px;
+  border-radius: 5px;
   transition: 0.3s;
   visibility: hidden;
   opacity: 0;
 }
-.action .menu.active {
-  top: 80px;
-  visibility: visible;
-  opacity: 1;
-}
+// .action .menu.active {
+//   top: 50px;
+//   visibility: visible;
+//   opacity: 1;
+// }
 .action .menu::before {
   content: "";
   position: absolute;
@@ -201,35 +244,90 @@ a {
   height: 20px;
   background-color: #fff;
   transform: rotate(45deg);
-  border-radius: 3px;
+  border-radius: 4px;
 }
 
 .action .menu ul li {
   margin: 0;
   list-style: none;
-  padding: 10px 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
   display: flex;
   align-items: center;
   justify-content: center;
 }
 .action .menu ul li img {
-  max-width: 20px;
-  margin-right: 20px;
-  opacity: 0.5;
   transition: 0.5s;
 }
-.action .menu ul li:hover img {
-  opacity: 1;
-}
+
 .action .menu ul li a {
   display: inline-block;
   color: #555;
+  font-size: 12px;
   font-weight: 500;
   transition: 0.5s;
 }
-.action .menu ul li:hover a {
-  color: #ff5d94;
+
+.ul-perfiles li {
+  padding-top: 5px;
 }
-*/
+.ul-perfiles button {
+  width: 111px;
+  height: 37px;
+  padding: 6px 6px 6px 7px;
+  border: 0;
+  border-radius: 18.5px;
+  // box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  background-color: white;
+  display: flex;
+  // justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.selected-user-image {
+  // border: 2px solid red;
+  background-color: #ce00ff;
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 0 2px;
+}
+.selected-user-button {
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+}
+.not-selected-user-button {
+  color: #9b9b9b;
+}
+.not-selected-user-image {
+  // border: 2px solid red;
+  background-color: #9b9b9b;
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 0 2px;
+}
+.ul-perfiles span {
+  font-family: "Montserrat";
+  font-size: 12px;
+  margin-left: 5px;
+}
+.ul-settings {
+  // border: 2px solid blue;
+}
+.ul-settings li {
+  // border: 2px solid blue;
+  display: flex !important;
+  justify-content: left !important;
+  padding: 7px 0 7px 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+.ul-settings li:last-child {
+  border-bottom: 0;
+}
 </style>
--- src/components/Proximamente.vue
+++ src/components/Proximamente.vue
@@ -11,20 +11,9 @@
         />
       </div>
     </div>
-    <!--
-    <div class="proximamente-wrapper-desktop bg-secondary">
-      <h4>Próximamente</h4>
-      <img
-        class="col-3"
-        v-for="(image, index) in images"
-        :key="index"
-        :src="image"
-        alt=""
-      />
-    </div>
-    -->
+
     <div class="proximamente-wrapper-desktop">
-      <h4>Próximamente</h4>
+      <!-- <h4>Próximamente</h4>
       <div class="carousel">
         <div class="carouselbox">
           <img
@@ -34,9 +23,16 @@
             alt=""
             :class="'img-' + index"
           />
-          <a class="switchLeft sliderButton">&lt;</a>
-          <a class="switchRight sliderButton">></a>
+          
         </div>
+      </div> -->
+      <div class="carousel">
+        <h1>Hola</h1>
+        <div class="carouselbox">
+          <!-- imagenes -->
+        </div>
+        <a class="switchLeft sliderButton">&lt;</a>
+        <a class="switchRight sliderButton">></a>
       </div>
     </div>
   </div>
@@ -103,83 +99,14 @@ export default {
 
 /* video */
 .carousel {
-  // height: 250px;
-  // width: 80%;
-  position: relative;
-}
-.carousel .switchLeft,
-.carousel .switchRight {
-  color: white;
-  font-weight: bold;
-  // height: 100%;
-  // width: 45px;
-  // line-height: 250px;
-  font-size: 25px;
-  text-align: center;
-  // top: 0;
-  z-index: 3;
-  text-decoration: none;
-}
-.carousel .switchLeft {
-  position: absolute;
-  left: 0; //!cuidado
-}
-.carousel .switchRight {
-  position: absolute;
-  right: 0; //!cuidado
-}
-.carouselbox {
-  // height: 250px;
-  width: auto;
-  overflow: hidden; //scroll
-  white-space: nowrap;
-  display: inline-block;
-  text-align: center;
-  // padding-bottom: 10px !important;
-  padding: 23px 0 0 0;
-  display: flex;
-  align-items: center;
-}
-.carouselbox img {
-  width: 255px;
-  height: 155px;
-  background-size: cover;
-  // margin: 5px 10px;
-  margin: 0 31px 0 0;
-  transition: 0.5s ease;
-  z-index: 2;
-}
-.carouselbox img:hover {
-  // transform: scale(1.4);
-  // z-index: 5;
-}
-/*
-img {
-  width: 255px;
-  height: 155px;
-  margin: 20.3px 31px 0 0;
-}
-
-.container {
   margin: 0;
-}
-
-.col-3 {
   padding: 0;
+  background-color: #262626;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 }
-
-p {
-  font-family: Montserrat;
-  font-size: 20px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: white;
-  margin-bottom: 0;
-}
-*/
 @media screen and (min-width: 760px) {
   .proximamente-wrapper-desktop {
     display: block;
