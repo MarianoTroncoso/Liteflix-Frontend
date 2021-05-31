986918d601320b2ba53f97c8293e3f2e95978a4e
Mariano <petotronco@gmail.com>
2021-05-31 08:32:33 -0300

    inicio hover imagen populares

--- src/components/ImagenPopulares.vue
+++ src/components/ImagenPopulares.vue
@@ -5,31 +5,25 @@
         <div class="content-overlay"></div>
         <img class="content-image" :src="image" />
         <div class="content-details fadeIn-bottom grid-container">
-          <div class="row-1">
-            <a href="#">
-              <img
-                src="../../static/add-list.svg"
-                alt=""
-                class="add-list-img"
-              />
-            </a>
-            <a href="#" class="like-a">
-              <img src="../../static/like.png" alt="" />
-            </a>
+          <div class="hover-content">
+            <div class="row-1">
+              <a href="#"><img src="../../static/play-2.svg" alt="" /></a>
+            </div>
+            <div class="row-2">
+              <div class="movie-info">
+                <p class="title">
+                  <b>{{ title }}</b>
+                </p>
+                <p class="desc">
+                  98% Coincidencia <span class="min-age">+16</span> 1h 30 min
+                </p>
+                <p class="category">Suspenso</p>
+              </div>
+              <a href="#" class="like-a">
+                <img src="../../static/like.png" alt="" />
+              </a>
+            </div>
           </div>
-          <div class="row-2">
-            <a href="#"><img src="../../static/play-2.svg" alt="" /></a>
-          </div>
-          <div class="row-3">
-            <p class="title">
-              <b>{{ title }}</b>
-            </p>
-            <p class="desc">
-              98% Coincidencia <span class="min-age">+16</span> 1h 30 min
-            </p>
-            <p class="category">Suspenso</p>
-          </div>
-          <img src="../../static/arrow-down.svg" alt="" class="arrow-down" />
         </div>
       </a>
     </div>
@@ -55,18 +49,11 @@ export default {
   padding: 0;
   // float: left;
   // width: 50%;
-  // border: 2px solid green;
+
   margin-right: 23px;
 }
 
-// .container .title {
-//   color: #1a1a1a;
-//   text-align: center;
-//   margin-bottom: 10px;
-// }
-
 .content {
-  // border: 2px solid red;
   position: relative;
   // width: 90%;
   // max-width: 400px;
@@ -78,9 +65,8 @@ export default {
 }
 
 .content .content-overlay {
-  // border: 2px solid blue;
   // background: rgba(0, 0, 0, 0.7); //! difuminado
-  box-shadow: 0 -130px 80px -60px black inset;
+  box-shadow: 0 -150px 80px -60px black inset;
   position: absolute;
   height: 100%; //! estaba en 99%
   width: 100%;
@@ -92,7 +78,6 @@ export default {
   -webkit-transition: all 0.4s ease-in-out 0s;
   -moz-transition: all 0.4s ease-in-out 0s;
   transition: all 0.4s ease-in-out 0s;
-  padding: 0;
 }
 
 .content:hover .content-overlay {
@@ -120,6 +105,7 @@ export default {
   -webkit-transition: all 0.3s ease-in-out 0s;
   -moz-transition: all 0.3s ease-in-out 0s;
   transition: all 0.3s ease-in-out 0s;
+  padding: 336px 0 0 0;
 }
 
 .grid-container {
@@ -133,76 +119,69 @@ export default {
   font-size: 12px;
 }
 
-.row-1 {
+.hover-content {
   display: grid;
-  grid-template-columns: auto auto;
-  justify-content: end;
-  align-content: center;
-  padding: 8px 8px 0 0;
-}
-
-.add-list-img {
-  width: 32px;
-  height: 32px;
-}
-
-.like-a {
-  width: 32px;
-  height: 32px;
-  border: 1.5px solid white;
-  border-radius: 17px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 10px;
-  padding: 15px;
-}
-
-.like-a img {
-  width: 20px;
-  height: 18px;
-  margin-bottom: 4px;
-}
-
-.row-2 {
-  // background-color: blue;
-  display: grid;
-  grid-template-columns: auto;
-  justify-items: center;
-  padding: 0 0 0 0;
-}
-.row-3 {
-  // background-color: green;
   color: white;
-  padding: 0 0 10px 10px;
-}
-.row-3 p {
-  margin: 0;
-  font-size: 12px;
-}
-.row-3 .title {
-  font-size: 15px;
-}
-.min-age {
-  width: 35px;
-  height: 16px;
-  border-radius: 9.5px;
-  background-color: gray;
-  padding: 0 2px 0 2px;
-  font-family: "Montserrat";
-  padding: 0 8px 0 8px;
-}
 
-.arrow-down {
-  margin: 10px auto 0 auto;
+  p {
+    margin: 0;
+  }
 }
-
 .content:hover .content-details {
   top: 50%;
   // left: 50%;
   opacity: 1;
 }
+.row-1 {
+  // border: 2px solid green;
+  padding: 0 0 0 12px;
+}
+.row-2 {
+  // border: 2px solid blue;
+  display: grid;
+  grid-template-columns: 50% 50%;
 
+  .movie-info {
+    // background-color: red;
+    font-family: "Montserrat";
+    font-size: 12px;
+    padding: 0 0 0 12px;
+    .title {
+      font-size: 15px;
+    }
+
+    .min-age {
+      width: 35px;
+      height: 16px;
+      border-radius: 9.5px;
+      background-color: gray;
+      padding: 0 2px 0 2px;
+      font-family: "Montserrat";
+      padding: 0 8px 0 8px;
+    }
+  }
+  .like-a {
+    width: 32px;
+    height: 32px;
+    border: 1.5px solid white;
+    border-radius: 17px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    // margin-left: 50px;
+    margin: 3px 0 0 50px;
+
+    img {
+      width: 20px;
+      height: 18px;
+      margin-bottom: 4px;
+    }
+
+    // display: flex;
+    // align-items: center;
+    // justify-content: center;
+  }
+}
 .content-details p {
   // color: #fff;
   // font-size: 0.8em;
